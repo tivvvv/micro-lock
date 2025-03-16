a9e43a632a98e8189f100392e5f94e4855fe0b5e
tivvvv <18612553935@163.com>
2025-03-16 22:28:48 +0800

    feat: 定义基本结构.

---
+++ src/main/java/com/tiv/lock/Main.java
@@ -0,0 +1,37 @@
+package com.tiv.lock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final int[] count = {1000};
+        List<Thread> threads = new ArrayList<>();
+        MyLock lock = new MyLock();
+
+        for (int i = 0; i < 100; i++) {
+            threads.add(new Thread(() -> {
+                lock.lock();
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        Thread.sleep(2);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    count[0]--;
+                }
+                lock.unlock();
+            }));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println(count[0]);
+    }
+}
---
+++ src/main/java/com/tiv/lock/MyLock.java
@@ -0,0 +1,12 @@
+package com.tiv.lock;
+
+/**
+ * 自定义锁
+ */
+public class MyLock {
+    void lock() {
+    }
+
+    void unlock() {
+    }
+}
